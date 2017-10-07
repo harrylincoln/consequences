3cc31bb3a119a7ffbf0351b39b5469dad5b0cd7b
Lincoln <h.lincoln@elsevier.com>
2017-10-07 17:06:36 +0100

    tweak the proxy issue

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true // needed because it's hosted on Heroku
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id })
