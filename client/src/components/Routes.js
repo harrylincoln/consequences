cbd699d0726ea82814c394418bd2cfd796bc5bc3
Lincoln <h.lincoln@elsevier.com>
2017-11-06 17:13:09 +0000

    remove map stuff

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-
-import Plot from './views/plot';
 
 class Routes extends Component {
   render() {
     return (
-      <Route exact path="/routes/plot" component={Plot} />
-    );
+      <div></div>
+    )
   }
 };
 
