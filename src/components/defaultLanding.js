d0cdf4fbd438421b2807ec13caff0c01010e8570
Lincoln <h.lincoln@elsevier.com>
2017-12-27 13:36:16 +0000

    working array work

--- src/components/defaultLanding.js
+++ src/components/defaultLanding.js
@@ -5,22 +5,21 @@ class DefaultLanding extends Component {
   constructor(props) {
     super(props);
     this.todaysDate = new Date().toISOString().slice(0, 10);
-    this.state = { games: [] };
+    this.state = { players: [] };
   }
-  componentWillMount() {
-    // let gamesRef = fire.database().ref('games/' + this.todaysDate);
-    // gamesRef.on('child_added', snapshot => {
-    // })
+  componentWillMount(){
+    /* Create reference to messages in Firebase Database */
+    let playersRef = fire.database().ref('players').orderByKey().limitToLast(100);
+    playersRef.on('child_added', snapshot => {
+      /* Update React state when message is added at Firebase Database */
+      let player = { text: snapshot.val(), id: snapshot.key };
+      this.setState({ players: [player].concat(this.state.players) });
+    })
   }
-  addGame(e){
+  addPlayer(e){
     e.preventDefault(); // <- prevent form submit from reloading the page
     /* Send the message to Firebase */
-    fire.database().ref('games/' + this.todaysDate + '/players/').push({
-      name: this.inputEl.value, age: '30'
-    })
-    // fire.database().ref('games/' + this.todaysDate).set({
-    //     players: {name: this.inputEl.value, age: '30'}
-    // });
+    fire.database().ref('players').push( this.inputEl.value );
     this.inputEl.value = ''; // <- clear the input
   }
 
@@ -30,13 +29,13 @@ class DefaultLanding extends Component {
       <h2>
       Please enter your name
       </h2>
-      <form onSubmit={this.addGame.bind(this)}>
+      <form onSubmit={this.addPlayer.bind(this)}>
         <input type="text" ref={ el => this.inputEl = el }/>
         <input type="submit"/>
-        { this.state.games.length ? <p style={{textAlign:'center'}}>People ready</p> : null }
+        { this.state.players.length ? <p style={{textAlign:'center'}}>People ready</p> : null }
         <ul>
           { /* Render the list of messages */
-            this.state.games.map( game => <li key={game.id}>{game.text}</li> )
+            this.state.players.map( player => <li key={player.id}>{player.text}</li> )
           }
         </ul>
       </form>
