5ea656bc785a3fb0aef720d8b4c2545b6e2fc87a
Lincoln <h.lincoln@elsevier.com>
2018-08-30 21:10:08 +0100

    Revert "heroku envs"
    This reverts commit ef05c711972b53e0be52c5afd5eabc9757ba2c44.

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
-const fbConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+const config = {
+  apiKey: "AIzaSyBNJk_7_ff0m9tr6Vhh6yXK30KD6EDnPuw",
+  authDomain: "consequences-cf53e.firebaseapp.com",
+  databaseURL: "https://consequences-cf53e.firebaseio.com",
+  projectId: "consequences-cf53e",
+  storageBucket: "consequences-cf53e.appspot.com",
+  messagingSenderId: "1034371850744"
 };
 
-export default firebase.initializeApp(fbConfig);
+export default firebase.initializeApp(config);
